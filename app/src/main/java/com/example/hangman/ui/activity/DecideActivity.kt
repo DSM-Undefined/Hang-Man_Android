e5edfb0fdd8bd7cef8c94b0d87092403f821c6a8
qwsd1109 <qwsd1109@gmail.com>
2019-09-23 23:16:48 +0900

    keyboard setting

--- app/src/main/java/com/example/hangman/ui/activity/DecideActivity.kt
+++ app/src/main/java/com/example/hangman/ui/activity/DecideActivity.kt
@@ -3,11 +3,16 @@ package com.example.hangman.ui.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.hangman.R
+import kotlinx.android.synthetic.main.activity_decide.*
 
 class DecideActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_decide)
+
+        tv_kb_a.setOnClickListener(){
+            tv_kb_a.setText("dp")
+        }
     }
 }
