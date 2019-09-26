098c92b2d12ff3e56db1331754073d4c605e20e2
qwsd1109 <qwsd1109@gmail.com>
2019-09-26 15:47:21 +0900

    activity_dicide finsh(maybe)

--- app/src/main/java/com/example/hangman/ui/activity/DecideActivity.kt
+++ app/src/main/java/com/example/hangman/ui/activity/DecideActivity.kt
@@ -2,17 +2,43 @@ package com.example.hangman.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.Window
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.TextView
 import com.example.hangman.R
 import kotlinx.android.synthetic.main.activity_decide.*
+import java.security.AccessController.getContext
 
-class DecideActivity : AppCompatActivity() {
+class DecideActivity : AppCompatActivity(), View.OnClickListener {
+    override fun onClick(p0: View?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_decide)
-
-        tv_kb_a.setOnClickListener(){
-            tv_kb_a.setText("dp")
+        btn_submit.setOnClickListener{
+            val shack : Animation = AnimationUtils.loadAnimation(this,R.anim.dicide_an_error)
+            ed_Questions_word.startAnimation(shack)
         }
+        /*
+        tv_kb_a.setOnClickListener(this)
+        tv_kb_b.setOnClickListener(this)
+        tv_kb_c.setOnClickListener(this)
+        tv_kb_d.setOnClickListener(this)
+        tv_kb_e.setOnClickListener(this)
+        tv_kb_f.setOnClickListener(this)
+        tv_kb_g.setOnClickListener(this)
+        tv_kb_h.setOnClickListener(this)
+        tv_kb_i.setOnClickListener(this)
+        tv_kb_j.setOnClickListener(this)
+        tv_kb_k.setOnClickListener(this)
+        tv_kb_l.setOnClickListener(this)
+        tv_kb_m.setOnClickListener(this)
+        tv_kb_n.setOnClickListener(this)
+        tv_kb_o.setOnClickListener(this)
+        tv_kb_p.setOnClickListener(this)*/
     }
 }
