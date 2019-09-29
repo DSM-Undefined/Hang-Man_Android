9b61eec147d1b93b32712e532f700ba6fa213ae2
MKGOLD <dkqk0124@naver.com>
2019-09-29 21:48:53 +0900

    [Add] Add Splash Activity

---
+++ app/src/main/java/com/example/hangman/ui/activity/SplashActivity.kt
@@ -0,0 +1,21 @@
+package com.example.hangman.ui.activity
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import java.io.IOException
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val intent = Intent(this, DecideActivity::class.java)
+        startActivity(intent)
+
+        try {
+            Thread.sleep(1000)
+        } catch (e: IOException) { }
+
+        finish()
+    }
+}
