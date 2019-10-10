f80912e7901b720bfd2e8f62e133ddb1ea8a4ad7
MKGOLD <dkqk0124@naver.com>
2019-10-10 20:31:16 +0900

    [Add] Androidx Material Migrate, Presenter

---
+++ app/src/main/java/com/example/hangman/presenter/SignupPresenter.kt
@@ -0,0 +1,9 @@
+package com.example.hangman.presenter
+
+import com.example.hangman.contract.SignupContract
+
+class SignupPresenter : SignupContract.Presenter {
+    override fun checkRegisterData(vararg text: String) {
+
+    }
+}
