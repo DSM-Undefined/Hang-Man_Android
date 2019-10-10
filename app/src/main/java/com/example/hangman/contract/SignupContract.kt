f83efd62872d270f6f51d0a2da733159f3a87eac
MKGOLD <dkqk0124@naver.com>
2019-10-10 20:32:46 +0900

    [Update] Signin Signup 로직 작성, MainActivity onclick 작성

--- app/src/main/java/com/example/hangman/contract/SignupContract.kt
+++ app/src/main/java/com/example/hangman/contract/SignupContract.kt
@@ -2,10 +2,11 @@ package com.example.hangman.contract
 
 interface SignupContract {
     interface View {
-
+        fun errorToast(text: String)
+        fun buttonAnimation()
     }
 
     interface Presenter {
-
+        fun checkRegisterData(vararg text : String)
     }
 }
