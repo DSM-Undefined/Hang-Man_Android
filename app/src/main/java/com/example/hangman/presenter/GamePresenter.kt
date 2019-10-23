ba25b02e88481121398f48d8e21249d69badb6f6
seung min <3235tmdals@gmail.com>
2019-10-23 20:26:34 +0900

    [Modify]: RoomActivity xml(toolbar) 수정

--- app/src/main/java/com/example/hangman/presenter/GamePresenter.kt
+++ app/src/main/java/com/example/hangman/presenter/GamePresenter.kt
@@ -4,8 +4,8 @@ import com.example.hangman.contract.GameroomContract
 import com.example.hangman.ui.activity.GameroomActivity
 
 class GamePresenter(private val view: GameroomActivity) : GameroomContract.Presenter {
-    override fun alphabetsOnClick(text: String) {
-
-    }
+//    override fun alphabetsOnClick(text: String) {
+//
+//    }
 
 }
